ce78748d2c249b332bb4db26b7ec68b882a1fbce
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2023-07-11 16:55:02 +0100

    Custom app bar created

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:webview_flutter_web/webview_flutter_web.dart';
 import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
-import 'package:frontend/screens/details_screen.dart';
+// import 'package:frontend/screens/details_screen.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -13,7 +13,7 @@ final theme = ThemeData(
     seedColor: const Color.fromARGB(255, 63, 17, 177),
   ),
   visualDensity: VisualDensity.adaptivePlatformDensity,
-  textTheme: GoogleFonts.latoTextTheme(),
+  textTheme: GoogleFonts.josefinSansTextTheme(),
 );
 
 // main.dart
@@ -27,8 +27,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // var screenSize = MediaQuery.of(context).size;
-    // print(screenSize);
+    var screenSize = MediaQuery.of(context).size;
+    print(screenSize);
     return MaterialApp(
       title: 'Explore',
       theme: theme,
--- frontend/lib/screens/catalogue_screen.dart
+++ frontend/lib/screens/catalogue_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/widgets/appBar.dart';
+import 'package:frontend/widgets/app_bar.dart';
 
 class CatalogueScreen extends StatefulWidget {
   const CatalogueScreen({super.key});
@@ -11,10 +11,32 @@ class CatalogueScreen extends StatefulWidget {
 class _CatalogueScreenState extends State<CatalogueScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Stack(
-      children: [
-        CustomAppBar(),
-      ],
+    return const Scaffold(
+      body: Stack(
+        children: <Widget>[
+          // this will be fill the entire screen
+          // Positioned.fill(
+          //   child: Image.network('https://i.imgur.com/SJGDZUp.png'),
+          // ),
+
+          Positioned(
+            top: 85,
+            child: Expanded(
+              child: Center(
+                child: Text('Body of my catalogue'),
+              ),
+            ),
+          ),
+
+          // App Bar
+          Positioned(
+            top: 15,
+            left: 50,
+            right: 50,
+            child: CustomAppBar(),
+          ),
+        ],
+      ),
     );
   }
 }
